1e096d44fac2e790a8af22ebab2f231c015f5d6f
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2020-12-24 00:29:40 +0700

    done menus page

---
+++ datafake/menuimage.js
@@ -0,0 +1,85 @@
+import SrcImg1 from "../public/static/img/image-menu-page-1.jpg";
+import SrcImg2 from "../public/static/img/image-menu-page-2.jpg";
+import SrcImg3 from "../public/static/img/image-menu-page-3.jpg";
+import SrcImg4 from "../public/static/img/image-menu-page-4.jpg";
+import SrcImg5 from "../public/static/img/image-menu-page-5.jpg";
+import SrcImg6 from "../public/static/img/image-menu-page-6.jpg";
+import SrcImg7 from "../public/static/img/ENG_P_DEALOFTHEWEEK_800PX-01-copy-4.png";
+import SrcImg8 from "../public/static/img/TH_WebApp_YearEndHolidayMenu-01.png";
+import SrcImg9 from "../public/static/img/TH_WebApp_LunchMenu_800-1.png";
+import SrcImg10 from "../public/static/img/TH_DineInMenu_3.9_2020-01-copy-1.png";
+import SrcImg11 from "../public/static/img/BKK-Wine-list.12.8FN-1_Artboard-2-copy.png";
+import SrcImg12 from "../public/static/img/TH_Drink.png";
+import SrcImg13 from "../public/static/img/TH_CIGAR_10.10_800PX-01.png";
+const ImageMenuPages = {
+    ImageMenuSlider: [
+        {
+            "id": 1,
+            "name": "image 1",
+            "imgUrl": SrcImg1
+        },
+        {
+            "id": 2,
+            "name": "image 2",
+            "imgUrl": SrcImg2
+        },
+        {
+            "id": 3,
+            "name": "image 3",
+            "imgUrl": SrcImg3
+        },
+        {
+            "id": 4,
+            "name": "image 4",
+            "imgUrl": SrcImg4
+        },
+        {
+            "id": 5,
+            "name": "image 5",
+            "imgUrl": SrcImg5
+        },
+        {
+            "id": 6,
+            "name": "image 6",
+            "imgUrl": SrcImg6
+        },
+
+    ], ImageMenuBigSize: [
+        {
+            "id": 7,
+            "name": "image 7",
+            "imgUrl": SrcImg7
+        },
+        {
+            "id": 8,
+            "name": "image 8",
+            "imgUrl": SrcImg8
+        },
+        {
+            "id": 9,
+            "name": "image 9",
+            "imgUrl": SrcImg9
+        },
+    ], ImageDiningMenu: {
+        "id": 10,
+        "name": "image 10",
+        "imgUrl": SrcImg10
+    }
+    , WineTrasures: {
+        "id": 11,
+        "name": "image 11",
+        "imgUrl": SrcImg11
+    }
+    , Drink: {
+        "id": 12,
+        "name": "image 12",
+        "imgUrl": SrcImg12
+    }
+    , Cigar: {
+        "id": 13,
+        "name": "image 13",
+        "imgUrl": SrcImg13
+    }
+}
+
+export default ImageMenuPages;
