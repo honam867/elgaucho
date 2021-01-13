83f1e9eb5ae167d4eebf9ae05f2b192617ab1fb9
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2021-01-14 02:09:54 +0700

    setting redux persist successful

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import App from 'next/app';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import ReactDOM from 'react-dom';
 import '../.semantic/dist/semantic.min.css';
@@ -9,38 +8,39 @@ import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css';
 import Router from 'next/router';
 import { wrapper } from '../redux/store'
+import { PersistGate } from "redux-persist/integration/react";
+import { useStore } from 'react-redux';
 NProgress.configure(
   {
     template: '<div id="nprogress"><div class="bar" role="bar" style="background: #cf1b15; height: 3px"></div>',
     spinnerSelector: false
   },
-
-
 )
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-export class MyApp extends App {
-  componentDidMount() {
+function MyApp({ Component, pageProps }) {
+  const store = useStore((state) => state);
+  useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       const axe = require('react-axe')
       axe(React, ReactDOM, 1000)
     }
-  }
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <title>El Gaucho Vietnam</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </Head>
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>El Gaucho Vietnam</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+      </Head>
+      <PersistGate persistor={store.__persistor} >
         <Component {...pageProps} />
-      </>
-    )
-  }
+      </PersistGate>
+    </>
+  );
 }
-export default wrapper.withRedux(MyApp)
+
+export default wrapper.withRedux(MyApp);
