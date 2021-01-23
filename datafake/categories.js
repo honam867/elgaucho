d80908f487c0514afe177e385b673e44555d6987
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2021-01-23 18:44:10 +0700

    fix api

---
+++ datafake/categories.js
@@ -0,0 +1,20 @@
+const Categories =
+  [
+    {
+      "id": 1,
+      "name": "Soups~Starters-Salads"
+    },
+    {
+      "id": 2,
+      "name": "Burgers~Pastas"
+    },
+    {
+      "id": 3,
+      "name": "Beef~Steaks"
+    },
+    {
+      "id": 4,
+      "name": "Lamb~Pork~Chiken~Fish"
+    }
+  ]
+export default Categories;
---
+++ datafake/subcategories.js
@@ -0,0 +1,30 @@
+const SubCategories =
+  [
+    {
+      "id": 1,
+      "name": "Soup",
+      "categoryId": 1
+    },
+    {
+      "id": 2,
+      "name": "Starters",
+      "categoryId": 1
+    },
+    {
+      "id": 3,
+      "name": "Salads",
+      "categoryId": 1
+    },
+    {
+      "id": 4,
+      "name": "El Gaucho Burger",
+      "categoryId": 2
+    },
+    {
+      "id": 5,
+      "name": "Pastas",
+      "categoryId": 2
+    },
+
+  ]
+export default SubCategories;
